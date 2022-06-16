d7a11c479b555bc3afdaf5a3f1e6d5731102401d
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-16 19:18:33 -0300

    rename getDb with getCollection

--- src/infra/helpers/mongo-helper.js
+++ src/infra/helpers/mongo-helper.js
@@ -1,17 +1,27 @@
 const { MongoClient } = require('mongodb')
 
 module.exports = {
-  async connect (uri) {
+  async connect (uri, dbName) {
+    this.uri = uri
+    this.dbName = dbName
     this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
-    this.db = this.client.db()
+    this.db = this.client.db(dbName)
   },
 
   async disconnect () {
     await this.client.close()
     this.client = null
     this.db = null
+  },
+
+  async getCollection (name) {
+    if (!this.client) {
+      await this.connect(this.uri, this.dbName)
+    }
+
+    return this.db.collection(name)
   }
 }
