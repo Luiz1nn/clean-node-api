1ef36f5268815923c1dcb2c58e5a59178b0f92e7
Luis Fernando <luisfernandobr2003@gmail.com>
2022-06-16 17:39:28 -0300

    add routes file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongoDb://localhost:27017/clean-node-api'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api'
 }
