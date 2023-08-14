1d5b0072735c966750bc95654f081dd8837c58d9
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-14 09:25:17 -0300

    test(BcryptAdapter): ensure calls bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it, vi } from 'vitest'
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from './bcrypt-adapter'
+
+describe('Bcrypt Adapter', () => {
+  it('should call bcrypt with correct values', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hashSpy = vi.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+})
