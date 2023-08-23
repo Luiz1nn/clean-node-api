45440b89c4785d4eb755664f247d836aaa2d7cf4
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-23 15:58:58 -0300

    test(LoginController): ensure returns 401 if invalid credentials are provided

--- src/presentation/controllers/login.spec.ts
+++ src/presentation/controllers/login.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it, vi } from 'vitest'
 import { LoginController } from './login'
-import { badRequest, serverError } from '../helpers'
+import { badRequest, serverError, unauthorized } from '../helpers'
 import { InvalidParamError, MissingParamError } from '../errors'
 import type { EmailValidator, HttpRequest } from '../protocols'
 import { type Authentication } from '~/domain'
@@ -98,5 +98,12 @@ describe('Login Controller', () => {
     const authSpy = vi.spyOn(authenticationStub, 'auth')
     await sut.handle(makeFakeRequest())
     expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password')
+  })
+
+  it('should return 401 if invalid credentials are provided', async () => {
+    const { sut, authenticationStub } = makeSut()
+    vi.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve('null')))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(unauthorized())
   })
 })
