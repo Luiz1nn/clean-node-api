f4c45a720db7228a49c6ba3d27f033bd2b0037e5
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-25 11:01:08 -0300

    test: name in decribe

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { RequiredFieldValidation } from './required-field-validation'
 import { MissingParamError } from '~/presentation/errors'
 
-describe('', () => {
+describe('RequiredField Validation', () => {
   it('should return a MissingParamError if validation fails', () => {
     const sut = new RequiredFieldValidation('field')
     const error = sut.validate({ name: 'any_name' })
