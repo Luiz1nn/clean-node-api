712845e4e403aec0e384a8ced540f6a8b15541e7
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-29 09:01:56 -0300

    test(BcryptAdapter): ensure calls bcrypt compare with correct values

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,20 +2,33 @@ import { describe, expect, it, vi } from 'vitest'
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+vi.mock('bcrypt', async () => {
+  const actual: typeof bcrypt = await vi.importActual('bcrypt')
+  return {
+    ...actual,
+    async hash (): Promise<string> {
+      return await new Promise(resolve => resolve('hash'))
+    },
+    async compare (): Promise<boolean> {
+      return await new Promise(resolve => resolve(true))
+    }
+  }
+})
+
 const salt = 12
 const makeSut = (): BcryptAdapter => {
   return new BcryptAdapter(salt)
 }
 
 describe('Bcrypt Adapter', () => {
-  it('should call bcrypt with correct values', async () => {
+  it('should call hash with correct values', async () => {
     const sut = makeSut()
     const hashSpy = vi.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  it('should return a hash on success', async () => {
+  it('should return a valid hash on hash success', async () => {
     const sut = makeSut()
     vi.spyOn(bcrypt, 'hash').mockImplementation(async () => {
       return await new Promise(resolve => { resolve('hash') })
@@ -29,5 +42,12 @@ describe('Bcrypt Adapter', () => {
     vi.spyOn(bcrypt, 'hash').mockImplementation(async () => await new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.hash('any_value')
     await expect(promise).rejects.toThrow()
+  })
+
+  it('should call compare with correct values', async () => {
+    const sut = makeSut()
+    const compareSpy = vi.spyOn(bcrypt, 'compare')
+    await sut.compare('any_value', 'any_hash')
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
   })
 })
