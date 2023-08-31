9114d3e842d939a59f8f79478fe5231fb3d61f7c
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-31 18:17:22 -0300

    chore: add tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  outDir: 'dist',
+  entry: ['src', '!src/**/*.spec.ts', '!src/**/*.test.ts'],
+  splitting: false,
+  clean: true
+})
