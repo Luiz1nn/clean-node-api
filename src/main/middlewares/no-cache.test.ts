b371aaee7c5fdc1916916a55959ce7eada7e94d0
Luis Fernando <luisfernandobr2003@gmail.com>
2023-10-11 10:42:30 -0300

    test: add no cache middleware

---
+++ src/main/middlewares/no-cache.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import type { Express } from 'express'
+import { beforeAll, describe, it } from 'vitest'
+import { noCache } from './no-cache'
+import { setupApp } from '../config'
+
+let app: Express
+
+describe('NoCache Middleware', () => {
+  beforeAll(async () => {
+    app = await setupApp()
+  })
+
+  it('should disable cache', async () => {
+    app.get('/test_no_cache', noCache, (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_no_cache')
+      .expect('cache-control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+      .expect('expires', '0')
+      .expect('surrogate-control', 'no-store')
+  })
+})
