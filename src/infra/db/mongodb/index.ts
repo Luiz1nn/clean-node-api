4e5adcef42eb5998d4464ea77ad8ff61f2ed9684
Luis Fernando <luisfernandobr2003@gmail.com>
2023-10-16 17:59:16 -0300

    refactor: use builder patterns to build the aggregation query

--- src/infra/db/mongodb/index.ts
+++ src/infra/db/mongodb/index.ts
@@ -1,5 +1,6 @@
 export * from './account/account-mongo-repository'
 export * from './helper/mongo-helper'
+export * from './helper/query-builder'
 export * from './log/log-mongo-repository'
 export * from './survey/survey-mongo-repository'
 export * from './survey-result/survey-result-mongo-repository'
