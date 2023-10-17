c457f821f0722137048155c24bdac7c2feab17c6
Luis Fernando <luisfernandobr2003@gmail.com>
2023-10-17 17:31:07 -0300

    test(LoadSurveyResultController): ensure returns 403 if an invalid surveyId is provided

--- src/presentation/controllers/survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result-controller.spec.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it, vi } from 'vitest'
 import type { LoadSurveyById } from '~/domain/usecases'
+import { InvalidParamError } from '~/presentation/errors'
+import { forbidden } from '~/presentation/helpers'
 import type { HttpRequest } from '~/presentation/protocols'
 import { mockLoadSurveyById } from '~/presentation/test'
 import { LoadSurveyResultController } from './load-survey-result-controller'
@@ -30,5 +32,12 @@ describe('LoadSurveyResult Controller', () => {
     const loadByIdSpy = vi.spyOn(loadSurveyByIdStub, 'loadById')
     await sut.handle(mockRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  it('should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    vi.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 })
