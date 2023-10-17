a402fce59e9e6aab7fde453771f95d64410b77e8
Luis Fernando <luisfernandobr2003@gmail.com>
2023-10-17 17:36:01 -0300

    test(LoadSurveyResultController): ensure returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result-controller.spec.ts
@@ -1,10 +1,11 @@
 import { describe, expect, it, vi } from 'vitest'
 import type { LoadSurveyById } from '~/domain/usecases'
 import { InvalidParamError } from '~/presentation/errors'
-import { forbidden } from '~/presentation/helpers'
+import { forbidden, serverError } from '~/presentation/helpers'
 import type { HttpRequest } from '~/presentation/protocols'
 import { mockLoadSurveyById } from '~/presentation/test'
 import { LoadSurveyResultController } from './load-survey-result-controller'
+import { throwError } from '~/domain/test'
 
 const mockRequest = (): HttpRequest => ({
   params: {
@@ -39,5 +40,12 @@ describe('LoadSurveyResult Controller', () => {
     vi.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
+  })
+
+  it('should return 500 if LoadSurveyById throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    vi.spyOn(loadSurveyByIdStub, 'loadById').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
